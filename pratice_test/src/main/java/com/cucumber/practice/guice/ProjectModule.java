947acb77591ad9dd9af74a0996831b5f223100a0
Divya Sai Perecharla <pdivyasai248@gmail.com>
2025-08-22 20:10:03 +0530

    commit

---
+++ pratice_test/src/main/java/com/cucumber/practice/guice/ProjectModule.java
@@ -0,0 +1,14 @@
+package com.cucumber.practice.guice;
+
+import com.cucumber.practice.guice.provider.WebDriverProvider;
+
+import com.google.inject.AbstractModule;
+import com.google.inject.Singleton;
+import org.openqa.selenium.WebDriver;
+
+public final class ProjectModule extends AbstractModule {
+    @Override
+    public void configure() {
+        bind(WebDriver.class).toProvider(WebDriverProvider.class).in(Singleton.class);
+    }
+}
---
+++ pratice_test/src/test/java/com/cucumber/practice/acceptancetests/AllTests.java
@@ -0,0 +1,16 @@
+package com.cucumber.practice.acceptancetests;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        glue = {"com.cucumber.practice"},
+        features = ".",
+        format = {"pretty", "json:target/cucumber-reports/cucumber.json"},
+        tags = {"@complete"}
+)
+public class AllTests {
+}
